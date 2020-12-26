6d0db64f385e21a6aebbee106066b9d2cae7bdd5
Patmosk <38389110+Patmosk@users.noreply.github.com>
2020-12-26 04:57:17 +0100

    Add files via upload

---
+++ funcion compleja.py
@@ -0,0 +1,13 @@
+def operacion (var1 , var2):
+
+	resultado = var1 * var2
+
+	return resultado
+
+contenedor1=(operacion(5,10))
+contenedor2=(operacion(3,6))
+
+total=contenedor1+contenedor2
+print(total)
+#print(contenedor1)
+#print(contenedor2)
